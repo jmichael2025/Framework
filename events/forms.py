b211a3f22228afb2a1383eee6fd5bf75094f7dda
jmichael2025 <jasminmichael8@gmail.com>
2026-08-11 14:49:51 +0100

    Add create event form

--- events/forms.py
+++ events/forms.py
@@ -5,3 +5,8 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = '__all__'
+
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+        }
