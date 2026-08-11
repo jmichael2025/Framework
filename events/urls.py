0352ea3bbbdf4f1c4a58ad2907121108644b10de
jmichael2025 <jasminmichael8@gmail.com>
2026-08-11 14:19:21 +0100

    Add event browsing and creation functionality

---
+++ events/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('events/', views.event_list, name='event_list'),
+    path('events/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('events/create/', views.create_event, name='create_event'),
+]
--- events/views.py
+++ events/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from.models import Event 
+from .forms import EventForm
 
-# Create your views here.
+
+def home(request):
+    events = Event.objects.all()
+    return render(request, 'events/home.html', {'events': events})
+
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'events/event_list.html', {'events': events})
+
+def event_detail(request, event_id):
+    event = Event.objects.get(id=event_id)
+    return render(request, 'events/event_detail.html', {'event': event})
+
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('event_list')
+    else:
+        form = EventForm()
+    return render(request, 'events/create_event.html', {'form': form})
